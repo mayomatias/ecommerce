2fe6b74234aed76fd525ae8c1bfe5744c5408ac9
mayomatias <mayomatiass@gmail.com>
2023-05-13 18:53:06 -0300

    Trabajando en nav var state

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,16 +1,35 @@
 import React from 'react';
 import "../../css/style.css"
+import { useState } from 'react';
 
 import CartWidget from '../CartWidget/CartWidget';
 
+
+
+
 const NavBar = () =>{
+  const [navBarState, setNavBarSatate] = useState(0)
+  const imprimir = () => {
+    
+    console.log("Hola desde una funcion")
+    if(navBarState == 0) {
+      console.log("Nav en 0")
+      setNavBarSatate(1);
+    } else {
+      setNavBarSatate(0);
+    }
+  }
+  
   return( 
     <nav>
-      <ul className="drop-menu">
+      <ul className="drop-menu" id='nav-drop-menu'>
         <li className="nav-item"><a>Productos</a></li>
         <li className="nav-item"><a>Nosotros</a></li>
         <li className="nav-item"><a>Ayuda</a></li>
       </ul>
+      <div>
+        <a id='hamburguer-menu' onClick={imprimir} >HAMB</a>  
+      </div>
       <div id='search-box'>
         <div />
         <input type='text' />
