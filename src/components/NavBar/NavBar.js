c96aa1125b062d5be9ff06f5b8556662b48be5d7
Matias M <mayomatiass@gmail.com>
2023-05-14 22:30:32 -0300

    NavBar drop down menu

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import "../../css/style.css"
 import { useState } from 'react';
-
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
 import CartWidget from '../CartWidget/CartWidget';
+
 let dropDowMenu = "nav-drop-menu-invisible" 
 
-
-
 const NavBar = () =>{
-  const [navBarState, setNavBarSatate] = useState(0)
+  const [navBarState, setNavBarSatate] = useState(1)
   const imprimir = () => {
     console.log("Hola desde una funcion")
     if(navBarState == 0) {
@@ -21,22 +21,26 @@ const NavBar = () =>{
   }
   
   return( 
-    <nav>
-      <ul className={dropDowMenu} id='nav-drop-menu'>
-        <li className="nav-item"><a>Productos</a></li>
-        <li className="nav-item"><a>Nosotros</a></li>
-        <li className="nav-item"><a>Ayuda</a></li>
-      </ul>
-      <div>
-        <a id='hamburguer-menu' onClick={imprimir} >HAMB</a>  
-      </div>
-      <div id='search-box'>
-        <div />
-        <input type='text' />
-        <div />
-      </div>
-      <CartWidget />
-    </nav>
+    <header>
+      <h1>GammerShop</h1>
+      <nav>
+        <ul className={dropDowMenu} id='nav-drop-menu'>
+          <li className="nav-item"><a>Productos</a></li>
+          <li className="nav-item"><a>Nosotros</a></li>
+          <li className="nav-item"><a>Ayuda</a></li>
+        </ul>
+        <div>
+          {navBarState ? <MenuIcon onClick={imprimir} /> : <CloseIcon onClick={imprimir} />}
+        
+        </div>
+        <div id='search-box'>
+          <div />
+          <input type='text' />
+          <div />
+        </div>
+        <CartWidget />
+      </nav>
+    </header>
   );
 }
 
